06c50b9dc71da93bae1ed031e61e52ad86b02419
Lars Erik Finholt <lars.erik.finholt@bouvet.no>
2018-09-21 19:26:42 +0200

    center

--- src/views/Firstrun.vue
+++ src/views/Firstrun.vue
@@ -1,16 +1,21 @@
 <template>
-   <v-container fluid>
-      <v-layout column align-center>
+   <v-container fluid fill-height>
+      <v-layout row wrap align-center >
+        <v-flex>
+          <v-layout column align-center>
         <v-flex>
           <v-dialog v-model="dialog" persistent max-width="500px">
-            <v-btn slot="activator" color="primary" dark>Kom igang</v-btn>
+            <v-btn slot="activator" large color="success" dark>Kom igang</v-btn>
             <v-card>
               <v-card-title>
-                <span class="headline">Basisinformasjon</span>
+                <span v-if="step==0" class="headline">Steg 1 - Basisinformasjon</span>
+                <span v-if="step==1" class="headline">Steg 2 - Familemedlemmer</span>
               </v-card-title>
               <v-card-text>
                 <v-container grid-list-md>
                   <v-layout wrap>
+                    <v-flex v-if="step==0">
+                      <v-layout wrap>
                     <v-flex xs12 sm6 >
                       <v-text-field label="Fornavn (ditt navn)" required  v-model="persons[0].name"></v-text-field>
                     </v-flex>
@@ -21,21 +26,7 @@
                       ></v-text-field>
                     </v-flex>
                     <v-flex xs12>
-                      <v-text-field label="Email (din epost)" required v-model="persons[0].email"></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 v-for="(person,index) of persons" :key="index">
-                      <v-layout>
-                      <v-flex xs12 sm6 >
-                        <v-text-field label="Fornavn" required v-model="person.name"></v-text-field>
-                      </v-flex>
-                      <v-flex xs12 sm6>
-                        <v-select v-model="person.age"
-                          :items="['0-5', '6-12', '13-25', '25+']"
-                          label="Age"
-                          required
-                        ></v-select>
-                      </v-flex>
-                      </v-layout>
+                      <v-text-field label="Epost (din epost)" required v-model="persons[0].email"></v-text-field>
                     </v-flex>
                     <v-flex xs12 sm12>
                       <v-autocomplete
@@ -45,20 +36,50 @@
                         chips
                       ></v-autocomplete>
                     </v-flex>
+                      </v-layout>
+                    </v-flex>
+                   <v-flex v-if="step==1">
+                      <v-layout wrap>
+                    <v-flex xs12 v-for="(person,index) of persons" :key="index">
+                      <v-layout>
+                      <v-flex xs12 sm6 >
+                        <v-text-field label="Fornavn" required v-model="person.name"></v-text-field>
+                      </v-flex>
+                      <v-flex xs12 sm6>
+                        <v-select v-model="person.age"
+                          :items="['0-5', '6-12', '13-25', '25+']"
+                          label="Alder"
+                          required
+                        ></v-select>
+                      </v-flex>
+                      </v-layout>
+                    </v-flex>
+                    <v-flex xs12>
+                      <v-layout justify-end="">
+                      <v-btn flat v-if="persons[persons.length-1].name" small @click="add()">Legg til</v-btn>
+                      <v-btn flat color="error" v-if="persons.length>3" small @click="remove()">Fjern </v-btn>
+                      </v-layout>
+                    </v-flex>
+                      </v-layout>
+                   </v-flex>
                   </v-layout>
                 </v-container>
-                <small>*indicates required field</small>
+                <!-- <small>*indicates required field</small> -->
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
-                <v-btn color="blue darken-1" flat @click.native="dialog = false">Fortsett</v-btn>
+                <v-btn  v-if="step==0" flat @click.native="dialog = false">Avbryt</v-btn>
+                <v-btn v-if="step==0" color="success"  @click.native="step = 1">Neste</v-btn>
+                <v-btn  v-if="step==1" flat @click.native="step = 0">Forrige</v-btn>
+                <v-btn v-if="step==1" color="success"  @click.native="dialog = false">Ferdig</v-btn>
               </v-card-actions>
             </v-card>
           </v-dialog>
         </v-flex>
         <v-flex>
           <v-btn small>Allerede bruker?</v-btn>
+        </v-flex>
+          </v-layout>
         </v-flex>
       </v-layout>
    </v-container>
@@ -70,6 +91,8 @@ import Person from "../components/Person";
 export default {
   data() {
     return {
+      dialog:false,
+      step:0,
       family: {},
       persons: [
         {
@@ -91,7 +114,9 @@ export default {
     add() {
       this.persons.push({ name: null, age: null });
     },
-    removePerson() {}
+    remove() {
+      this.persons.splice(-1,1);
+    }
   },
 
   computed: {
